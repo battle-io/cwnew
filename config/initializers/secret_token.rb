b43473db95c1ca5f0921478b5e25826eab859519
Ryan Cook <ryan@quickleft.com>
2011-06-04 13:45:15 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cwnew::Application.config.secret_token = '85a1351339b91c6111f7e328e0424dad4f56ad85e45846915d1bdb2df16f1100b1b7a322d1eeee478446f874c08f25074b68a7db02976e1c4c30c4892369f3ec'

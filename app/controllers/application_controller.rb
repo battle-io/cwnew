7251aab3a0a1b1ffcb362f6fa8710e6113c836ef
Ryan Cook <ryan@quickleft.com>
2011-06-05 00:50:56 -0600

    get rid of authentication override

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find_by_id( session["user_id"] ) || User.first
+    @current_user ||= User.find_by_id( session["user_id"] )
   end
   helper_method :current_user
 

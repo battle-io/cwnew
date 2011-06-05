578d49b89241d27c25ee931bc52d04989a8c6700
Ryan Cook <ryan@quickleft.com>
2011-06-05 16:35:40 -0600

    prep for open sourcing

---
+++ config/initializers/omniauth_template.rb
@@ -0,0 +1,7 @@
+# Setup our omniauth middleware
+# Move this file to config/initializers/omniauth.rb
+#
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :github , 'REPLACE_WITH_YOUR_CLIENT_ID' , 'REPLACE_WITH_YOUR_CLIENT_SECRET'
+end
+
